7d0843b0d3fd4b112d38d26c3b9af2d43d9ce40f
lidordvir <lidor4001@gmail.com>
2023-12-15 01:06:45 +0200

    * Added background image to city item

--- src/components/CityItem/CityItem.tsx
+++ src/components/CityItem/CityItem.tsx
@@ -1,6 +1,8 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import React, {FC} from 'react';
 import {ICity} from '../../utills/types';
+import {Image} from '../Image/Image';
+import {COLORS} from '../../assets/colors/colors';
 
 interface IProps {
   item: ICity;
@@ -10,6 +12,13 @@ interface IProps {
 export const CityItem: FC<IProps> = React.memo(({item, onPress}) => {
   return (
     <Pressable style={styles.container} onPress={onPress}>
+      <Image
+        style={styles.image}
+        resizeMode={'cover'}
+        source={{
+          uri: 'https://d1x6rln6sla7wg.cloudfront.net/clubs-images/67ff16c7-841b-43b3-928a-d728be45cb4d.jpg',
+        }}
+      />
       <View style={styles.contentContainer}>
         <Text style={styles.cityName}>City name: {item.name}</Text>
         <Text style={styles.countryName}>Country name: {item.country}</Text>
@@ -32,12 +41,24 @@ const styles = StyleSheet.create({
   cityName: {
     textAlign: 'center',
     marginBottom: 12,
+    color: COLORS.white,
   },
   countryName: {
     textAlign: 'center',
     marginBottom: 12,
+    color: COLORS.white,
   },
   description: {
     textAlign: 'center',
+    color: COLORS.white,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
   },
 });
